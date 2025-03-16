18a567f3e4c2bcc9cf428606d90fcdaa4a0dedc4
unknown <Favourduru24@gmail.com>
2025-03-15 21:32:23 -0700

    Completed:

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -24,9 +24,9 @@ import CategoryFilter from "@/components/shared/CategoryFilter";
          console.log(event)
      return (
         <>
-         <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-             <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-                <div className="flex flex-col justify-center gap-8">
+         <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10 px-5 sm:px-10">
+             <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0 ">
+                <div className="flex flex-col justify-center gap-8 ">
                   <h1 className="h1-bold">Share, Engage and celebrate: Your Event, Our Platform!</h1>
                    <p className="p-regular-20 md:p-regular-24 ">
                    Expert Advice from 5,000+ Mentors in Top Companies with Our Global Communities!
@@ -38,9 +38,9 @@ import CategoryFilter from "@/components/shared/CategoryFilter";
                  <Image src="/assets/images/hero.png" width={1000} height={1000} alt="hero" className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"/>
              </div>
          </section>
-          <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+          <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12 px-5 sm:px-10">
           <h2 className="h2-bold">Driving Innovation For <br/> Events Everywhere</h2>
-          <div className="flex w-full flex-col gap-5 md:flex-row">
+          <div className="flex w-full flex-col gap-5 md:flex-row px-5 sm:px-10">
               <Search/>
               <CategoryFilter/>
                
